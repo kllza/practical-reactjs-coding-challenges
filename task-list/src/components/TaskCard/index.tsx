98af5ec8e195891705103271fc2e2360f821443b
kllza <carmartinez9901@gmail.com>
2023-06-29 10:29:28 -0400

    status & progress-bar

--- task-list/src/components/TaskCard/index.tsx
+++ task-list/src/components/TaskCard/index.tsx
@@ -1,31 +1,56 @@
-import classNames from "classnames"
-import { ReactComponent as DeleteIcon } from "../../assets/icons/delete.svg"
-import { ReactComponent as EditIcon } from "../../assets/icons/edit.svg"
-import CircularProgressBar from "../CircularProgressBar"
-import "./style.scss"
+import classNames from "classnames";
+import { ReactComponent as DeleteIcon } from "../../assets/icons/delete.svg";
+import { ReactComponent as EditIcon } from "../../assets/icons/edit.svg";
+import CircularProgressBar from "../CircularProgressBar";
+import "./style.scss";
+import { useState } from "react";
 
 interface Task {
-  id: string
-  title: string
-  priority: string
-  status: string
-  progress: number
+  id: string;
+  title: string;
+  priority: string;
+  progress: number;
 }
+
 interface TaskCardProps {
-  task: Task
-  onEditTask: (taskId: string) => void
-  onDeleteTask: (taskId: string) => void
+  task: Task;
+  onEditTask: (taskId: string) => void;
+  onDeleteTask: (taskId: string) => void;
 }
 
 const TaskCard: React.FC<TaskCardProps> = ({ task, onEditTask, onDeleteTask }) => {
-  const { id, title, priority, status, progress } = task
+  const { id, title, priority} = task;
 
   const handleEditClick = () => {
-    onEditTask(id)
-  }
+    onEditTask(id);
+  };
 
   const handleDeleteClick = () => {
-    onDeleteTask(id)
+    onDeleteTask(id);
+  };
+
+  const [currentStatus, setCurrentStatus] = useState("To Do");
+
+  const handleStatusClick = () => {
+    let newStatus = "";
+
+    if (currentStatus === "To Do") {
+      newStatus = "In Progress";
+    } else if (currentStatus === "In Progress") {
+      newStatus = "Done";
+    } else if (currentStatus === "Done") {
+      newStatus = "To Do";
+    }
+
+    setCurrentStatus(newStatus);
+  };
+
+  let progressBarPercentage = 0;
+
+  if (currentStatus === "In Progress") {
+    progressBarPercentage = 50;
+  } else if (currentStatus === "Done") {
+    progressBarPercentage = 100;
   }
 
   return (
@@ -39,17 +64,19 @@ const TaskCard: React.FC<TaskCardProps> = ({ task, onEditTask, onDeleteTask }) =
         <span className={classNames(`${priority}-priority`, "priority")}>{priority}</span>
       </div>
       <div className="task-status-wrapper">
-        <button className="status">{status}</button>
+        <button className="status" onClick={handleStatusClick}>
+          {currentStatus}
+        </button>
       </div>
       <div className="progress">
-        <CircularProgressBar strokeWidth={2} sqSize={24} percentage={progress} />
+        <CircularProgressBar strokeWidth={2} sqSize={24} percentage={progressBarPercentage} />
       </div>
       <div className="actions">
         <EditIcon className="mr-20 cp" onClick={handleEditClick} />
         <DeleteIcon className="cp" onClick={handleDeleteClick} />
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default TaskCard
+export default TaskCard;
